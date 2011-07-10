b883673b675caa8d5db58777a862cb33e3a0ac32
Dave Simons <dave@inuits.be>
2011-07-10 06:04:47 +0200

    virtualize update sequences
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/pre.pp
+++ manifests/pre.pp
@@ -1,22 +1,22 @@
 class redmine::pre {
+	@exec {
+		'gem update':
+			command => 'gem update --system',
+			path => '/usr/bin:/opt/ruby/bin';
+
+		'apt update':
+			command => 'apt-get update',
+			path => '/usr/bin';
+
+		'yum update':
+			command => 'yum update',
+			path => '/usr/bin';
+	}
+
+#	realize(Exec['gem update'])
+
 	case $operatingsystem {
-		Centos: { include redmine::pre::centos }
-		Debian: { include redmine::pre::debian }
-	}
-
-	class centos {
-		exec {
-			'yum update':
-				command => 'yum update',
-				path => '/usr/bin',
-		}
-	}
-
-	class debian {
-		exec {
-			'apt update':
-				command => 'apt-get update',
-				path => '/usr/bin';
-		}
+		Centos: {realize(Exec['yum update'])}
+		Debian: {realize(Exec['apt update'])}
 	}
 }
