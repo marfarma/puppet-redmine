ba3c28823b43dd3f3a2125360f2d6c708e8e5a7c
Dave Simons <dave@inuits.be>
2011-08-12 15:37:23 +0200

    take out all the ugly db stuff
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,5 @@
 class redmine::config {
 
-	$production_db="redmine_production"
-	$development_db="redmine_development"
-	$redmine_db_user="redmine"
-	$redmine_db_pass="redmine"
-
 	file { 'database.yml':
 		ensure => present,
 		owner => $redmine_id,
@@ -23,40 +18,13 @@ class redmine::config {
 		group => $redmine_id,
 	}
 
-	exec { 'config_redmine_production_db':
-		command =>  '/usr/bin/mysqladmin -uroot create redmine_production',
-		unless => '/bin/echo "show databases"|mysql -uroot|grep "redmine_production"',
-	}
-
-	exec { 'config_redmine_development_db':
-		command =>  '/usr/bin/mysqladmin -uroot create redmine_development',
-		unless => '/bin/echo "show databases"|mysql -uroot|grep "redmine_development"',
-#		require => Exec['config_redmine_production_db'],
-	}
-
-	exec { 'config_redmine_mysql_user':
-		command =>  '/bin/echo "create user \'redmine\'@\'localhost\' identified by \'redmine\'"|mysql -uroot',
-		unless => '/bin/echo "select user from mysql.user where user=\'redmine\'"|mysql -uroot|grep redmine',
-		require => Exec['config_redmine_development_db'],
-	}
-
-	exec { 'config_redmine_production_perms':
-		command => '/bin/echo "grant all on redmine_production.* to \'redmine\'@\'localhost\'"|mysql -uroot',
-		require =>  Exec["config_redmine_mysql_user"],
-	}
-
-	exec { 'config_redmine_devel_perms':
-		command => '/bin/echo "grant all on redmine_development.* to \'redmine\'@\'localhost\'"|mysql -uroot',
-		require => Exec['config_redmine_production_perms'],
-	}
-
 	exec { 'config_redmine_mysql_bootstrap':
 		environment => 'RAILS_ENV=production',
 		path => '/usr:/usr/bin',
 		cwd => '/usr/share/redmine',
 		provider => shell,
 		command => 'rake db:migrate',
-		require => Exec['config_redmine_devel_perms'],
+		require => Mysql_db[$production_db],
 		notify => Service["$redmine::webserver"];
 	}
 }
