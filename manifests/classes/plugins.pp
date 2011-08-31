8d831afd270ee85b67c4cfbad2402446da88b473
Dave Simons <dave@inuits.be>
2011-08-31 14:42:32 +0200

    add gitolite plugin/change gitosis repo url
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/plugins.pp
+++ manifests/classes/plugins.pp
@@ -1,8 +1,13 @@
 class redmine::plugins {
 	@redmine::plugin {
+		'gitolite':
+			name => 'gitolite',
+			url => 'https://github.com/DarwinAwardWinner/redmine_git_hosting.git',
+			gems => ['lockfile', 'inifile', 'net-ssh'];
+
 		'gitosis':
 			name => 'redmine_gitosis',
-			url => 'https://github.com/DarwinAwardWinner/redmine_git_hosting.git',
+			url => 'https://github.com/y8/redmine-gitosis.git',
 			gems => ['lockfile', 'inifile', 'net-ssh'];
 	}
 
