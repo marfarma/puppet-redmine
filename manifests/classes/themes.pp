fa7b625ed27dd7d3c9730ec02c917dd2f766c93e
Dave Simons <dave@inuits.be>
2011-09-09 01:03:47 +0200

    add aurora theme
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/themes.pp
+++ manifests/classes/themes.pp
@@ -2,6 +2,9 @@ class redmine::themes {
 	@redmine::theme {
 		'pepper':
 			url => 'https://github.com/koppen/redmine-pepper-theme.git';
+
+		'aurora':
+			url => 'https://github.com/jorgebg/redmine-aurora-theme.git';
 	}
 
 	if $redmine::themes {
