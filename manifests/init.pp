159c0cc94cf2d9a0911f955873c4317a8297667c
Dave Simons <dave@inuits.be>
2011-11-22 14:40:39 +0100

    take out stages - it was a dumb idea
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,6 @@ class redmine (
 	$devel_db = 'redmine_devel',
 	$dbuser = 'redmine',
 	$dbpass = 'redmine',
-	$stages = 'no',
 	$home = '/usr/share/redmine',
 	$plugins = [],
 	$mail_user = 'redmine',
@@ -19,42 +18,23 @@ class redmine (
 	$mail_tls = 'true',
 	$themes = []
 ) {
-	if $stages == 'no' {
-		class{
-			'redmine::pre':
-				before => Class['redmine::depends'];
-			'redmine::depends':
-				before => Class['redmine::core'];
-			'redmine::core':
-				before => Class['redmine::dbconf'];
-			'redmine::dbconf':
-				require => Service['mysqld'],
-				before => Class['redmine::config'];
-			'redmine::config':
-				before => Class['redmine::plugins'];
-			'redmine::plugins':;
-			'redmine::themes':;
-		}
-	} else {
-		class {
-			'redmine::pre':
-				stage => pre;
-			'redmine::depends':
-				stage => depends,
-				require => Class['ruby'];
-			'redmine::core':
-				stage => core;
-			'redmine::dbconf':
-				stage => config;
-			'redmine::config':
-				stage => config;
-			'redmine::plugins':
-				require => Class['redmine::config'],
-				stage => config;
-			'redmine::themes':
-				stage => config;
-		}
+	class {
+		'redmine::pre':
+			before => Class['redmine::depends'];
+		'redmine::depends':
+			before => Class['redmine::core'];
+		'redmine::core':
+			before => Class['redmine::dbconf'];
+		'redmine::dbconf':
+			require => Service['mysqld'],
+			before => Class['redmine::config'];
+		'redmine::config':
+			before => Class['redmine::plugins'];
+		'redmine::plugins':;
+		'redmine::themes':;
 	}
 
-    Exec{path=>'/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin'}
+    Exec {
+		path=>'/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin'
+	}
 }
