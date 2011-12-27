4321375bd4613e1788eb92dc849407899642441d
Dave Simons <dave@inuits.eu>
2011-12-27 16:42:46 +0100

    move selinux permissive setting to pre class
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/classes/core.pp
+++ manifests/classes/core.pp
@@ -1,11 +1,5 @@
 class redmine::core {
 	exec {
-		'selinux_permissive':
-			path => '/bin:/usr/bin:/usr/sbin',
-			command => 'setenforce permissive',
-			unless => 'cat /etc/selinux/config|grep "SELINUX=disabled"',
-			onlyif => 'which setenforce';
-
 		'session_store':
 			path => '/bin:/usr/bin:/opt/ruby/bin',
 			cwd => '/usr/share/redmine/public',
--- manifests/classes/pre.pp
+++ manifests/classes/pre.pp
@@ -13,4 +13,12 @@ class redmine::pre {
 		Centos: {realize(Exec['yum update'])}
 		Debian: {realize(Exec['apt update'])}
 	}
+
+	exec {
+		'selinux_permissive':
+			path => '/bin:/usr/bin:/usr/sbin',
+			command => 'setenforce permissive',
+			unless => 'cat /etc/selinux/config|grep "SELINUX=disabled"',
+			onlyif => 'which setenforce';
+	}
 }
