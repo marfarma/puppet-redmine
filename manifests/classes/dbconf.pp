4c9c6a9b2ca211d64a73b82fb63902344151496d
Dave Simons <dave@inuits.eu>
2012-01-03 11:59:41 +0100

    add command for loading default config into redmine
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/classes/dbconf.pp
+++ manifests/classes/dbconf.pp
@@ -27,5 +27,13 @@ class redmine::dbconf {
 			command => 'rake db:migrate',
 			require => Mysql_db[$redmine::production_db],
 			notify => Service["$apache::apache"];
+		'load_default_data':
+			environment => 'RAILS_ENV=production REDMINE_LANG=en',
+			path => "$ruby::bin_dir",
+			cwd => "$redmine::home",
+			provider => 'shell',
+			command => 'rake redmine:load_default_data',
+			require => [ Mysql_db[$redmine::production_db], Exec['config_redmine_mysql_bootstrap'] ],
+			notify => Service["$apache::apache"];
 	}
 }
